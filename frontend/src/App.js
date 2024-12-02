b186dbdfe0da9c91a48768659af6941d76e7df63
Saran J U <200701223@rajalakshmi.edu.in>
2024-12-01 20:08:34 -0500

    Revert "changes"
    This reverts commit e157d212d0fab0ee7ecc6d701fa04272082f43b0.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,48 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
 import { AuthProvider } from './AuthContext';
 import PrivateRoute from './PrivateRoute';
 import Navigation from "./Navigation/Nav";
 import Profile from "./Pages/Profile";
 import ApplicationHistory from "./Pages/ApplicationHistory";
 import HomePage from "./Pages/HomePage";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import AdminDashboard from "./components/AdminDashboard"; // Import AdminDashboard
+import Login from "./components/Login"
 import "./index.css";
+import Register from "./components/Register";
 
 function App() {
   
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute>
-                <HomePage />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/profile" element={
+    <Router>
+      <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+        <Route
+          path="/home"
+          element={
             <PrivateRoute>
-              <Navigation />
-              <div className="home-content"><Profile /></div>
+              <HomePage></HomePage>
             </PrivateRoute>
+          }
+        />
+        <Route path="/profile" element={
+          <PrivateRoute>
+          <Navigation />
+          <div className="home-content"><Profile /></div>
+          </PrivateRoute>
           } />
-          <Route path="/applicationHistory" element={
-            <PrivateRoute>
-              <Navigation />
-              <div className="home-content"><ApplicationHistory /></div>
-            </PrivateRoute>
+        <Route path="/applicationHistory" element={
+          <PrivateRoute>
+          <Navigation  />
+          <div className="home-content"><ApplicationHistory /></div>
+          </PrivateRoute>
           } />
+        
           <Route path="/login" element={<Login />} /> 
-          <Route path="/register" element={<Register />} />
-          
-          {/* Admin Route */}
-          <Route path="/admin" element={
-            <PrivateRoute>
-              <AdminDashboard />
-            </PrivateRoute>
-          } />
-          
-        </Routes>
-      </Router>
+          <Route path="/register" element={<Register />}/>
+        
+      </Routes>
+    </Router>
     </AuthProvider>
   );
 }
